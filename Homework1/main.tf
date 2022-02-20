9fcfd1ece0a435dc2cf9851766dfe14db1566516
ize1020 <49163323+ize1020@users.noreply.github.com>
2022-02-20 22:37:22 +0200

    Update main.tf

--- Homework1/main.tf
+++ Homework1/main.tf
@@ -1,8 +1,8 @@
-rovider "aws" {
+provider "aws" {
   profile = "default"
   region = "us-east-1"
 }
-resource "aws_instance" "isaac-lab" {
+resource "aws_instance" "grandpa-web" {
 ami = "ami-04505e74c0741db8d"
 instance_type ="t3.micro"
 tags = {
@@ -11,18 +11,17 @@ tags = {
  key_name = "ZOHAR"
   user_data = <<EOF
 #!/bin/bash -xe
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-sudo apt update -y
-sudo amazon-linux-extras install nginx1 -y
-sudo su -c "/bin/echo 'Welcome to Grandpa's Whiskey' >/usr/share/nginx/html/index.html"
-sudo systemctl start nginx
-sudo systemctl enable  nginx
+sudo apt-get install nginx -y
+sudo chmod 777 /var/www/html/index.nginx-debian.html
+sudo echo 'Welcome to Grandpa's Whiskey' >/var/www/html/index.nginx-debian.html
+sudo service nginx restart
 EOF
 }
 
 #step 2
-resource "aws_ebs_volume" "ebs1"{
- availability_zone = aws_instance.isaac-lab.availability_zone
+#create a resource from EBS volume in same AZ as os1
+resource "aws_ebs_volume" "ebs2"{
+ availability_zone = aws_instance.grandpa-web.availability_zone
  size = 10
  encrypted = true
  tags = {
@@ -33,7 +32,8 @@ resource "aws_ebs_volume" "ebs1"{
 
 #step 3 
 #attach volume
-resource "aws_volume_attachment" "attach_ebs_1"{
+resource "aws_volume_attachment" "attach_ebs_2"{
 device_name = "/dev/sdh"
-volume_id = aws_ebs_volume.ebs1.id
-instance_id =aws_instance.isaac-lab.id
+volume_id = aws_ebs_volume.ebs2.id
+instance_id =aws_instance.grandpa-web.id
+}
