fcf6b8db75ac73ae6d8acf86adf243aaf8578505
ize1020 <49163323+ize1020@users.noreply.github.com>
2022-03-06 16:48:35 +0200

    i edit new file s3.tf

--- Homework2/S3.tf
+++ Homework2/S3.tf
@@ -1,23 +1,9 @@
-resource "aws_s3_bucket" "osptest" {
-  bucket = "my-tf-osptest-bucket"
+resource "aws_s3_bucket" "web-log" {
+  bucket = "my-tf-web-log-bucket"
 }
 
-resource "aws_s3_bucket_acl" "osptest" {
-  bucket = aws_s3_bucket.osptest.id
+resource "aws_s3_bucket_acl" "web-log" {
+  bucket = aws_s3_bucket.web-log.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket"
-}
-
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
-}
-
-resource "aws_s3_bucket_logging" "osptest" {
-  bucket = aws_s3_bucket.osptest.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "log/"
