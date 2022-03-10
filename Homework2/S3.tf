e96a15fc57080f93d9e2bdce68598a35125a9238
ize1020 <49163323+ize1020@users.noreply.github.com>
2022-03-10 23:59:47 +0200

    Delete Homework2 directory

--- Homework2/S3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "web-log" {
-  bucket = "my-tf-web-log-bucket"
-}
-
-resource "aws_s3_bucket_acl" "web-log" {
-  bucket = aws_s3_bucket.web-log.id
-  acl    = "private"
-}
-
