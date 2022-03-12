d1ccc9bb24d138dca82f9bf9a12a33492bb62dfc
ize1020 <49163323+ize1020@users.noreply.github.com>
2022-03-12 22:15:01 +0200

    Update provider.tf

--- Homework2/Stage/provider.tf
+++ Homework2/Stage/provider.tf
@@ -1,24 +1,35 @@
+terraform {
+	backend "remote" {
+		organization = "itzhak-org"
+		workspaces {
+			name = "Stage"
+		}
+	}
+}
+
+
+terraform {
+	required_providers {
+		aws = {
+			source  = "hashicorp/aws"
+			version	= "~>3.74.3"
+		}
+	}
+	
+	required_version = ">= 0.14.9"
+}
+
 provider "aws" {
   profile = "default"
   region  = var.aws_region
 }
 
 
-terraform {
-  backend "s3" {
-    bucket         = "my-tf-web-log-bucket"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
-
-terraform {
--  backend "remote" {
-+  cloud {
-    organization = "itzhak-org"
-    workspaces {
-    name = "Stage"
-    }
-   }
- }
+#terraform {
+#  backend "s3" {
+#    bucket         = "my-tf-web-log-bucket"
+#    key            = "global/s3/terraform.tfstate"
+#    region         = "us-east-1"
+#    encrypt        = true
+#  }
+#}
